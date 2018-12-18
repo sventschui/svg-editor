9ca0a1365e280842c824c96aec65748e31dfc22d
Sven Tschui <github@sventschui.ch>
2018-12-18 15:09:23 +0100

    Fix D&D in firefox

--- src/lib/editor/index.js
+++ src/lib/editor/index.js
@@ -138,6 +138,7 @@ export default class Editor extends PureComponent<$Exact<Props>, State> {
         return { x: pt.x, y: pt.y };
       };
 
+      e.preventDefault();
       e.stopPropagation();
 
       if (this.props.onDragStart) {
